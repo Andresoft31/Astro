7db73d4a19921303af1bf298346fdec09f811a0c
Andres Canales <andrescamo02@gmail.com>
2026-02-27 12:08:08 -0600

    astro host

--- astroweb/settings.py
+++ astroweb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2*$j$8*c3ixcf7imi5j1+$%lll)6k0^fj$pz)vvqq&_71_*ovv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['astro-otbl.onrender.com']
 
 
 # Application definition
